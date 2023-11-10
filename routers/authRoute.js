6f83a3d9e88af5d21dcca2b2cad09b98143d3872
sameer78699 <sameerdastagir5@gmail.com>
2023-11-10 23:22:23 +0530

    git

--- routers/authRoute.js
+++ routers/authRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {registerController} from "../controllers/authController.js"
-
+import { loginController } from "../controllers/authController.js";
 //router object
 
 const router=express.Router();
@@ -10,4 +10,7 @@ const router=express.Router();
 //routing
 //register
 router.post('/register',registerController);
+
+//login
+router.post('/login',loginController);
 export default router;
