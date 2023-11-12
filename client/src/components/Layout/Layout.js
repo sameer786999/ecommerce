9ea0b7b8c58a26a76d6d50a6f35394075db8d3c2
sameer78699 <sameerdastagir5@gmail.com>
2023-11-12 18:45:13 +0530

    git

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import Footer from '../Footer';
 import Header from '../Header';
-const Layout = ({children}) => {
+import {Helmet} from 'react-helmet'
+const Layout = ({children,title,description,keywords,author}) => {
   return (
-    
-      <>
-      <Header/>
-        <main style={{minHeight:'70vh'}}>
-            {children}</main>
-        
-        <Footer></Footer>
-      
-   
+    <>
+      <Helmet>
+   <div>
+     <meta charset="UTF-8"/>
+  <meta name="description" content={description} />
+  <meta name="keywords" content={keywords} />
+  <meta name="author" content={author} />
+  <title>{title}</title>
+
+</div>
+
+       
+      </Helmet>
+      <Header />
+      <main style={{ minHeight: "70vh" }}>{children}</main>
+
+      <Footer></Footer>
     </>
   );
+}
+
+Layout.defaultProps={
+  title:"Ecommerce-App,shop now",
+  description:"mern stack",
+  keywords:"mern ,react,node,express",
+  author:"Sameer Shaikh"
 }
 
 export default Layout
