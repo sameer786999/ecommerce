e5693daf3cd06f445c32a81dd41ae2a2f1c52c7d
sameer78699 <sameerdastagir5@gmail.com>
2023-11-13 17:25:04 +0530

    init

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -22,26 +22,31 @@ const Register = () => {
     e.preventDefault();
    
     try{
-             const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/register`,{
-              name,email,password,phone,address
+             const res = await axios.post("/api/v1/auth/register", {
+               name,
+               email,
+               password,
+               phone,
+               address,
              });
-             if(res.data.success){
-              toast.success(res.data.message)
-              navigate('/login');
+             if (res && res.data.success) {
+               toast.success(res.data && res.data.message);
+               navigate("/login");
+             } else {
+               toast.error(res.data.message);
              }
-             else{
-              toast.error(res.data.message)
-             }
+
     }
     catch(error){
       console.log(error);
+        toast.error("Something went wrong");
+
     }
 
    }
 
      
   return (
-  
     <>
       <Layout>
         <div className="register ">
@@ -70,17 +75,6 @@ const Register = () => {
                 required
               />
             </div>
-            <div className="mb-3">
-              <input
-                type="text"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                className="form-control"
-                id="exampleInputEmail1"
-                placeholder="Enter Your Address"
-                required
-              />
-            </div>
 
             <div className="mb-3">
               <input
@@ -104,7 +98,17 @@ const Register = () => {
                 required
               />
             </div>
-
+            <div className="mb-3">
+              <input
+                type="text"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                className="form-control"
+                id="exampleInputEmail1"
+                placeholder="Enter Your Address"
+                required
+              />
+            </div>
             <button type="submit" className="btn btn-primary">
               Submit
             </button>
